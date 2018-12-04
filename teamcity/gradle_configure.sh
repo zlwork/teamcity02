04b6788ce6dfb41d380ca830bc4470d93dde0044
Dmitriy Krupskiy <zlwork@mail.ru>
2018-12-04 17:02:06 +0700

    Update gradle_configure.sh

--- teamcity/gradle_configure.sh
+++ teamcity/gradle_configure.sh
@@ -7,7 +7,7 @@
 #EMAIL_HOST="mail_host"
 #EMAIL_TO="mail_to"
 
-curl -o lastBuild.tmp "http://ci.it-olimp-tomsk.com:8080/app/rest/buildTypes/id:bt2/builds/status:SUCCESS" --user rest:rest
+curl -o lastBuild.tmp "http://ci.it-olimp-tomsk.com:8080/app/rest/buildTypes/id:bt2/builds/status:SUCCESS" --user rest:rest \
 last_commit=`xpath lastBuild.tmp  '/build/revisions/revision/@version'| awk -F"\"" '{print $2}'`
 
 echo "##Last commit = $last_commit"
